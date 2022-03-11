bdded01e8e8386d20dca7ac2f91bce8e9c14525c
bananlabs <paulerzst@gmail.com>
2022-03-11 13:24:03 -0300

    create the initial structure of the contrat

---
+++ erc165-test.sol
@@ -0,0 +1,13 @@
+/* -- These contracts is to document my learning
+   -- in Solidity Language. My introductio to 
+   -- ERC 165 standard. Ξ 
+*/
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "./erc165.sol";
+
+contract ERC165 is IERC165 {
+
+}
