ab142f3d65b3f38e60c0581e3c6d4f4f586fdc09
bananlabs <paulerzst@gmail.com>
2022-03-11 13:49:47 -0300

    adding suportsInterface and _registerInterface functions

--- ERC165-standard/erc165-test.sol
+++ ERC165-standard/erc165-test.sol
@@ -12,9 +12,19 @@ import "./erc165.sol";
 contract ERC165 is IERC165 {
    // create table for interfaces 
    mapping(bytes4 => bool) private _supportedInterfaces;
-   
+   // check if is a supported interface
+      function supportsInterface(bytes4 intefaceId) external override view returns (bool){
+         return _supportedInterfaces[intefaceId];
+    }
     // calculate fingerprint interface
     function caculateFigerprint() public view returns (bytes4){
        return bytes4(keccak256('supportsInteface(bytes4)'));
     }
+   // registers a specific interface
+   function _registerInterface(bytes4 interfaceId) public {
+      // check if is a valid interface
+      require(interfaceId != 0xffffffff, "ERC165: Invalid Interface!");
+      _supportedInterfaces[interfaceId] = true;
+   }
+ 
 }
