23733d4db764f4a7bed2387cb6e8caf0edd07823
bananlabs <paulerzst@gmail.com>
2022-03-12 18:33:26 -0300

    adding ping.sol file and creates contract 'Ping'

---
+++ ping.sol
@@ -0,0 +1,12 @@
+/* -- A simple contract that replies with 'pong'
+   -- when you enter 'ping'. This contract make
+   -- use of string manipulation which in solidity
+   -- could be costly in terms of gas fee.
+   -- Be careful when you use strings manipulation.
+   -- This is just an exercise. 
+   -- Its a good idea to use Remix IDE 
+   -- to run those contracts.
+*/
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
