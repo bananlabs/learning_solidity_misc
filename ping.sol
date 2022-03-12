031f43e823653a0ae3f8b819932d1c77d8b89970
bananlabs <paulerzst@gmail.com>
2022-03-12 18:41:10 -0300

    adding say pong funcionality

--- ping.sol
+++ ping.sol
@@ -10,3 +10,17 @@
 
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
+
+contract Ping {
+    // Function that replies with 'pong' when user writes 'ping' 
+    function sayPong(string memory _str) pure public returns(string memory){
+        // string comparison in solidity
+        if (keccak256(abi.encodePacked(_str)) == keccak256(abi.encodePacked("ping"))){
+            // string concatenation and using unicode to write emoji in solidity
+            return string(abi.encodePacked("Pong!", " ", unicode"💢")); 
+        } else {
+            string memory warning = "You must write 'ping'";
+            return warning;
+        }
+    }
+}
