76bc790fe445b56cd2a228d141880dd222cc8e33
bananlabs <paulerzst@gmail.com>
2022-03-18 19:58:32 -0300

    creating addingName function

---
+++ rand-name-v2.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+// This version will add a mapping table that associate
+// the name to the random number
+contract RandNameTwo {
+    
+    struct Names {
+        uint256 randomId;
+        string name;
+    }
+
+    // instantiate a Names object
+    Names[] public myName;
+    
+    mapping(string => uint256) public nameToRandomId;
+
+    uint totalDigits = 16;
+    // we need assure after the modulus operation that 
+    // we will get a 16 digit number
+    uint digitModulus = 10 ** totalDigits;
+
+    // generate a random ID from a person's name
+    function _genereateRandomId(string memory _name) internal view returns (uint){
+        uint rand = uint(keccak256(abi.encodePacked(_name)));
+        return rand % digitModulus;
+    }
+
+    function addingName(string memory _name) public {
+        uint256 _myID = _genereateRandomId(_name);
+        myName.push(Names(_myID, _name));
+        nameToRandomId[_name] = _myID;
+    }
+
+}
