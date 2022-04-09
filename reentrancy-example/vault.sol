ec5f9cb5446963c4a0c85957f11bd5bb528e1c43
unknown <paulerzst@gmail.com>
2022-04-09 19:57:50 -0300

    created helper function to check balance

--- reentrancy-example/vault.sol
+++ reentrancy-example/vault.sol
@@ -12,5 +12,23 @@ contract Vault {
         balances[msg.sender] += msg.value;
     }
 
-    
+    /// @notice withdraw the value from balance
+    // problem in this function: the update is after the external call
+    // which permit the reentrancy attack
+    function withdraw() public{
+        uint balance = balances[msg.sender];
+        require(balance > 0);
+
+        (bool sent, ) = msg.sender.call{value: balance}("");
+        require(sent, "Failed to send ether");
+
+        //FIXME updating balance after external call
+        balances[msg.sender] = 0; 
+    }
+
+    /// @notice helper function to check contract balance
+    /// @return Return the current value of the balance
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
 }
