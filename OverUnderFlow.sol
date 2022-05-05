5fce0adbc60223f41a465064c7a21e13cb709baf
unknown <paulerzst@gmail.com>
2022-05-05 09:17:17 -0300

    create underflow and overflow functions

--- OverUnderFlow.sol
+++ OverUnderFlow.sol
@@ -9,4 +9,16 @@ pragma solidity ^0.8.0;
 contract OverUnderFlow {
     uint256 zero_num = 0;
     uint256 max_num = 2**(256-1);
+
+    /// @notice add 1 to the maximum value of uint. After addition the variable
+    // will be set to the zero.
+    function overflow() public {
+        max_num += 1;
+    }
+
+    /// @notice subtract 1 from zero. After subtraction the variable 
+    // will be set to the maximum value
+    function underflow() public {
+        zero_num -= 1;
+    }
 }
