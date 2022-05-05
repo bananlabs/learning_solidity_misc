3ab2c689b940f83c9bf4b35e9170149f270e2520
unknown <paulerzst@gmail.com>
2022-05-05 09:04:35 -0300

    create underflow-oveflow contract example

---
+++ OverUnderFlow.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+/// @title Example of overflow and underflow attacks
+/// @author @bananlabs
+/// @notice this contract is an example to simulate underflow and overflow
+/// in solidity. To fix this probem you have to use OpenZeppelin SafeMath
+
+contract OverUnderFlow {
+    
+}
