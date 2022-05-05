ad86f22223e7d100cac401cf749b55c41d964ae8
unknown <paulerzst@gmail.com>
2022-05-05 09:05:47 -0300

    create zero and max values variables

--- OverUnderFlow.sol
+++ OverUnderFlow.sol
@@ -7,5 +7,6 @@ pragma solidity ^0.8.0;
 /// in solidity. To fix this probem you have to use OpenZeppelin SafeMath
 
 contract OverUnderFlow {
-    
+    uint256 zero_num = 0;
+    uint256 max_num = 2**(256-1);
 }
